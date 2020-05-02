42aaa5de7252e5aff86522cc90786cfffa883ac4
Latino_Heat <vickeydreamss@gmail.com>
2020-05-02 21:10:05 +0530

    support vector machine algo

--- svm.py
+++ svm.py
@@ -1,10 +1,12 @@
 # support vector machine algo
+# here we are using SVC to be precise which is part of SVM
 
 import sklearn
 from sklearn import datasets
 from sklearn import svm
 import pandas as pd
 import numpy as np
+from sklearn import metrics
 
 cancer = datasets.load_breast_cancer()
 #print(cancer.feature_names)
@@ -20,3 +22,12 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y
 #print(y_test)
 
 classes = ['malignant' 'benign']
+
+clf =svm.SVC(kernel="linear")
+clf.fit(x_train, y_train)
+
+y_predict = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test, y_predict)
+print(acc)
+
+
